a849b417823ef701aedd393c69da33abb29d7108
Derek Townsend <derekjavon2@gmail.com>
2019-05-09 03:45:15 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,12 @@
 def reverse_each_word(string)
   elementArray=string.split(" ")
-  elementArray.each do |item|
-    item.replace(item.reverse)
-  end
+  # elementArray.each do |item|
+  #   item.replace(item.reverse)
+  # end
+  # elementArray.join(" ")
+
+  elementArray.collect{|item|
+    item.reverse
+  }
   elementArray.join(" ")
 end
